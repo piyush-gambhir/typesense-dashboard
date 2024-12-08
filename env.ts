48280d2441a609d69c608cd7483afeac8086a7af
Piyush Gambhir <90608533+piyush-gambhir@users.noreply.github.com>
2024-12-08 23:20:02 +0530

    refactor: Comment out Typesense environment variables in env.ts for clarity
    - Commented out the Typesense-related environment variables in env.ts to improve code readability and reduce clutter.
    - This change prepares the code for potential future modifications without removing the variables entirely.

--- env.ts
+++ env.ts
@@ -8,10 +8,10 @@ export const env = createEnv({
   server: {
     NODE_ENV: z.enum(['development', 'test', 'production']),
     ENVIRONMENT: z.enum(['development', 'test', 'production']),
-    TYPESENSE_HOST: z.string(),
-    TYPESENSE_PORT: z.string(),
-    TYPESENSE_PROTOCOL: z.string(),
-    TYPESENSE_API_KEY: z.string(),
+    // TYPESENSE_HOST: z.string(),
+    // TYPESENSE_PORT: z.string(),
+    // TYPESENSE_PROTOCOL: z.string(),
+    // TYPESENSE_API_KEY: z.string(),
   },
 
   /**
@@ -30,10 +30,10 @@ export const env = createEnv({
     NODE_ENV: process.env.NODE_ENV,
     ENVIRONMENT: process.env.ENVIRONMENT,
     NEXT_PUBLIC_APP_URL: process.env.NEXT_PUBLIC_APP_URL,
-    TYPESENSE_HOST: process.env.TYPESENSE_HOST,
-    TYPESENSE_PORT: process.env.TYPESENSE_PORT,
-    TYPESENSE_PROTOCOL: process.env.TYPESENSE_PROTOCOL,
-    TYPESENSE_API_KEY: process.env.TYPESENSE_API_KEY,
+    // TYPESENSE_HOST: process.env.TYPESENSE_HOST,
+    // TYPESENSE_PORT: process.env.TYPESENSE_PORT,
+    // TYPESENSE_PROTOCOL: process.env.TYPESENSE_PROTOCOL,
+    // TYPESENSE_API_KEY: process.env.TYPESENSE_API_KEY,
   },
   /**
    Run `build` or `dev` with `SKIP_ENV_VALIDATION` to skip env validation.
