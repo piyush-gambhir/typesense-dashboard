2be025b620cd8ec5f7bd54ef27ecdda619833f4f
piyush-gambhir <90608533+piyush-gambhir@users.noreply.github.com>
2025-07-12 03:46:02 +0530

    feat: Implement collection creation with JSONL file upload
    - Added CreateCollectionDialog component to facilitate the creation of new collections.
    - Introduced CreateCollectionWithJsonl component for handling JSONL file uploads and schema definition.
    - Updated collections page to include the new collection creation dialog.
    - Enhanced form validation and error handling for collection creation process.
    - Integrated Typesense collection creation logic with JSONL file import functionality.

--- app/(app)/collections/page.tsx
+++ app/(app)/collections/page.tsx
@@ -3,9 +3,17 @@ import React from 'react';
 import { getCollections } from '@/lib/typesense/collections';
 
 import TypesenseCollections from '@/components/collections/TypesenseCollections';
+import CreateCollectionDialog from '@/components/CreateCollectionDialog';
 
 export default async function page() {
   const collections = await getCollections();
 
-  return <TypesenseCollections collections={collections ?? []} />;
+  return (
+    <div className="container mx-auto flex flex-col gap-y-8">
+      <div className="flex justify-end p-8">
+        <CreateCollectionDialog />
+      </div>
+      <TypesenseCollections collections={collections ?? []} />
+    </div>
+  );
 }
